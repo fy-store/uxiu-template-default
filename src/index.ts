5dfab6b9d798bb8495f14dcfdf84119a6bd38706
feiYu <946686638@qq.com>
2025-04-01 20:14:31 +0800

    feat: 2.1.0

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { createApp } from 'uxiu'
 import { bodyParser } from '@koa/bodyparser'
 import cors from '@koa/cors'
 import koaStatic from 'koa-static'
-import Path from 'path/posix'
+import path from 'path/posix'
 
 const root = process.cwd()
 createApp({
@@ -10,11 +10,9 @@ createApp({
 	async mounted(ctx) {
 		ctx.app.use(cors())
 		ctx.app.use(bodyParser())
-		ctx.app.use(koaStatic(Path.join(root, '/public')))
+		ctx.app.use(koaStatic(path.join(root, '/public')))
 		const router = await import('@/router/index.js')
 		ctx.app.use(router.default.routes())
-		console.log('服务启动成功: http://localhost:3323')
-	},
+		console.log(`服务启动成功: http://localhost:${ctx.port}`)
+	}
 })
-
-
